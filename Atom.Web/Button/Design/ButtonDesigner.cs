0e8c7cd674c69273fc85a609ea67214fec2bfb2f
Ivan Baev <ivanbbaev@gmail.com>
2010-05-03 18:07:11 +0000

    Update poroject

---
+++ Atom.Web/Button/Design/ButtonDesigner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI.Design;
+
+namespace Atom.Web.UI.WebControls.Button.Desing
+{
+    class ButtonDesigner : ControlDesigner
+    {
+        public override string GetDesignTimeHtml()
+        {
+            return base.CreatePlaceHolderDesignTimeHtml();
+        }
+    }
+}
