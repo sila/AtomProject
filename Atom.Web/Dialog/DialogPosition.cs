e1eb57ce9faca9766ce5799781d63b9955cb8ab3
Ivan Baev <ivanbbaev@gmail.com>
2010-05-12 09:38:39 +0000

    add dialog postion enum

---
+++ Atom.Web/Dialog/DialogPosition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Atom.Web.UI.WebControls.Dialog
+{
+    public enum DialogPosition
+    {
+        Center,
+        CenterLeft,
+        CenterRight,
+        Left,
+        Right,
+        Top,
+        TopLeft,
+        TopRight,
+        Bottom,
+        BottomLeft,
+        BottomRight,
+    }
+}
