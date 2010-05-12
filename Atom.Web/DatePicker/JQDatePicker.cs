c2ac54233d4de327bf5af2762012a7179d0fa16e
Ivan Baev <ivanbbaev@gmail.com>
2010-05-12 20:36:33 +0000

    change name form DataPicker

---
+++ Atom.Web/DatePicker/JQDatePicker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI;
+using System.ComponentModel;
+using System.Web;
+using System.Security.Permissions;
+
+namespace Atom.Web.UI.WebControls.DatePicker
+{
+    [
+    AspNetHostingPermission(SecurityAction.Demand, Level = AspNetHostingPermissionLevel.Minimal),
+    AspNetHostingPermission(SecurityAction.InheritanceDemand, Level = AspNetHostingPermissionLevel.Minimal),
+    Designer(typeof(DatePickerDesigner)),
+    ToolboxData("<{0}:JQDatePicker runat=\"server\"> </{0}:JQDatePicker>")
+    ]
+    public class JQDatePicker
+    {
+    }
+}
