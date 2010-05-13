a09661eb019b6c11c4c8e09e0682227db48b623b
Ivan Baev <ivanbbaev@gmail.com>
2010-05-13 18:51:20 +0000

    add show animation animation

---
+++ Atom.Web/DatePicker/DatePickerAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Atom.Web.UI.WebControls.DatePicker
+{
+    public enum DatePickerAnimation
+    {
+        None,
+        Blind,
+        Bounce,
+        Clip,
+        Drop,
+        Explode,
+        Fold,
+        Highlight,
+        Puff,
+        Pulsate,
+        Scale,
+        Shake,
+        Size,
+        Slide,
+        Transfer
+    }
+}
